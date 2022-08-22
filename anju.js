b6fbe23c1fd8276c49a592da63647de1e0460011
anjana <anjanasreedega@gmail.com>
2022-08-22 16:32:01 +0530

    first commit

---
+++ anju.js
@@ -0,0 +1,23 @@
+var age ="24";
+age = Number(age)
+console.log(typeof age)
+console.log(age)
+
+var age = 25;
+console.log(age)
+console.log(typeof age)
+age = age.toString();
+console.log(age,typeof age)
+
+
+
+
+
+
+
+
+
+
+
+
+
