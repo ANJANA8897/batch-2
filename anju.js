bc5f066891fed9b140b33071a430ee990c3335df
anjana <anjanasreedega@gmail.com>
2022-08-23 12:11:28 +0530

    first commit

--- anju.js
+++ anju.js
@@ -1,24 +1,10 @@
 var name = "anjana";
 var age = 25;
-var married = "false";
-console.log(name,age,married)
-var gender ="female";
-var height = 5.5;
-var food = "chicken";
-console.log(gender)
-console.log(height)
-console.log(food)
-var english =70;
-var maths = 80;
-var science = 90;
-console.log(english+maths+science)
-var length = 30;
-var breadth = 20;
-console.log( perimeter = 2 * (length + breadth))
-console.log(area = length * breadth)
-var totalmonths= 24;
-var completedmonths = 5;
-console.log(totalmonths - completedmonths)
+var married = false;
+console.log(name)
+console.log(age)
+console.log(married)
+
 
 
 
